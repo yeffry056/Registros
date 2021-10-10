33536dca09066408c55d345a5c04014ced7eb35c
Jefferson Javier Vasquez <jefferson_javier@ucne.edu.do>
2021-10-10 01:23:49 -0400

    Add project files.

---
+++ Entidades/RolesDetalles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registros.Entidades
+{
+    public class RolesDetalles
+    {
+        [Key]
+        public int Id { get; set; }
+        public int RolId { get; set; }
+        public int PermisoId { get; set; }
+        public bool EsAsignado { get; set; }
+
+
+    }
+}
